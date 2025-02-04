8ad7de9e7a4cc8e9ea1d032876944951afbdec68
Aurelia <aureliajeffrina29@gmail.com>
2025-02-05 00:29:09 +0800

    scoring menu program done

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,19 +1,47 @@
+EXCELLENT_THRESHOLD = 90
+PASSABLE_THRESHOLD = 50
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+
 MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 
 def main():
     print(MENU)
+    score = None
     choice = input("Enter choice: ").upper()
     while choice != "Q":
         if choice == "G":
-            score = float(input("Enter a score: "))
-            while score < 0 and score > 100:
-                print("Invalid score")
-                score = float(input("Enter a score: "))
+            score = validate_number()
         elif choice == "P":
-            # TODO: copy or import your function to determine the result from score.py
+            score = get_valid_score(score)
+            print_result(score)
         elif choice =="S":
-            # TODO: this should print as many stars as the score
+            score = get_valid_score(score)
+            stars = "*" * score
+            print(stars)
         print(MENU)
         choice = input("Enter choice: ").upper()
     print("Finished.")
+
+def validate_number():
+    number = int(input("Enter a score: "))
+    while number < MINIMUM_SCORE or number > MAXIMUM_SCORE:
+        print("Invalid number")
+        number = int(input("Enter a score: "))
+    return number
+
+def get_valid_score(score):
+    if score is None:
+        print("Invalid score")
+        return validate_number()
+    return score
+
+def print_result(score):
+    if score >= EXCELLENT_THRESHOLD:
+        print(f"{score} is Excellent")
+    elif score >= PASSABLE_THRESHOLD:
+        print(f"{score} is Passable")
+    else:
+        print(f"{score} is Bad")
+
 main()
