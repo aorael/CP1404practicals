b3ed45818f6b9f5431d3fcacdc026841497b45a2
Aurelia <aureliajeffrina29@gmail.com>
2025-02-05 15:15:19 +0800

    converting temperatures program done

---
+++ prac_02/temperatures.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+def main():
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celcius = convert_to_celsius(fahrenheit)
+            print(f"Result: {celcius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def convert_to_fahrenheit(celsius):
+    return  5 / 9 * (celsius - 32)
+
+def convert_to_celsius(fahrenheit):
+    return fahrenheit * 9.0 / 5 + 32
+
+main()
