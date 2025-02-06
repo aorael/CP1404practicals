7851090185be4a818a38fc30cf5779c779a07b81
Aurelia <aureliajeffrina29@gmail.com>
2025-02-06 12:30:23 +0800

    score program done

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,12 +2,16 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASSABLE_THRESHOLD = 50
 
 def main():
     import random
 
     score = get_number()
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         return "Invalid score"
     else:
         message = get_message(score)
@@ -21,9 +25,9 @@ def get_number():
     return number
 
 def get_message(score):
-    if score >= 90:
+    if score >= EXCELLENT_THRESHOLD:
         return "Excellent"
-    elif score >= 50:
+    elif score >= PASSABLE_THRESHOLD:
         return "Passable"
     else:
         return "Bad"
