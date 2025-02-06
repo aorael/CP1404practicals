ff2be925e790c86d2ef045f1b2928ac03917cf37
Aurelia <aureliajeffrina29@gmail.com>
2025-02-06 17:25:21 +0800

    password stars program DONE

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -5,9 +5,11 @@ def main():
     print_asterisks(password)
 
 def print_asterisks(password):
+    """Printing asterisks based on the length of password"""
     print(password * "*")
 
 def get_password():
+    """Validating the password input by the user"""
     password = len(input("Enter password: "))
     while password <= PASSWORD_LENGTH:
         print("Invalid password")
