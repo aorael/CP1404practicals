4bfb3e5c2eaa0d2abe7bf60b7f3fa76b31a2d87a
Aurelia <aureliajeffrina29@gmail.com>
2025-02-10 17:58:55 +0800

    Answering three questions related to exceptional syntax DONE

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+# 1. ValueError occurs when a string is inputted to the numerator or denominator
+# 2. ZeroDivisionError occurs when 0 is inputted as the denominator
+# 3. Can, by using while loop to check the denominator so user can't input 0 as the denominator
