abd5fb18c1de06117b431aa6662a27226044c862
Aurelia <aureliajeffrina29@gmail.com>
2025-02-17 12:53:59 +0800

    Answering the questions regarding the errors in python and fixing the code DONE + correction

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -22,3 +22,23 @@ print("Finished.")
 # 1. ValueError occurs when a string is inputted to the numerator or denominator
 # 2. ZeroDivisionError occurs when 0 is inputted as the denominator
 # 3. Can, by using while loop to check the denominator so user can't input 0 as the denominator
+
+def correction():
+    is_valid = False
+    while not is_not_zero:
+        try:
+            numerator = int(input("Enter the numerator: "))
+            denominator = int(input("Enter the denominator: "))
+            while denominator == 0:
+                print("Cannot divide by zero!")
+                denominator = int(input("Enter the denominator: "))
+            fraction = numerator / denominator
+            print(fraction)
+            is_valid = True
+        except ValueError:
+            print("Numerator and denominator must be valid numbers!")
+        except ZeroDivisionError:
+            print("Cannot divide by zero!")
+    print("Finished.")
+
+correction()
