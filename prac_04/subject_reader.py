788e1063a8d7c2b36ff4fd68406431b02bcbb4ec
Aurelia <aureliajeffrina29@gmail.com>
2025-02-17 23:16:06 +0800

    CHECK load_data function read the warning

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,31 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
+    display_subject(data)
+
+def display_subject(data):
+    for i in range(len(data)):
+        subject = data[i][0]
+        name = data[i][1]
+        number_of_student = data[i][2]
+
+        print(f"{subject} is taught by {name} and has {number_of_student} students")
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+    # for line in input_file:
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
+        # line = line.strip()  # Remove the \n
+        # parts = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        # parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        # print("----------")
+        # print(line)
+    return ([line.strip().split(",") for line in input_file][:2])
     input_file.close()
-
 
 main()
