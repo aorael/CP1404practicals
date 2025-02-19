181824a72640e1466c3fb23a1c23959a78c28edf
Aurelia <aureliajeffrina29@gmail.com>
2025-02-19 22:32:46 +0800

    recheck

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,8 +1,23 @@
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+def main1():
+    numbers = []
+    for i in range(5):
+        number = int(input("Number: "))
+        numbers.append(number)
+    print(f"first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+main1()
+
+
+def main2():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-username = input("Enter username: ")
-if username in usernames:
-    print("Access granted")
-else:
-    print("Access denied")
+    username = input("Enter username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+main2()
