580bf73aa7b5e32386bef3d32a77ff6b671ff6d5
Aurelia <aureliajeffrina29@gmail.com>
2025-02-20 15:11:33 +0800

    quick picks program DONE

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,11 +1,18 @@
 import random
 
+MINIMUM_RANDOM_NUMBER = 1
+MAXIMUM_RANDOM_NUMBER = 45
+
+
 number_of_quick_picks = int(input("Enter number of quick picks: "))
 
-for i in range(number_of_quick_picks):
-    random_number = random.randint(1, 45)
+    for row in range(number_of_quick_picks):
+        numbers =[]
 
-    for j in range(6):
-        random_number = random.randint(1, 45)
-        print(f"{random_number:2}", end=" ")
-    print(f"{random_number:2}",sep="")
+        while len(numbers) < number_of_quick_picks:
+            random_number = random.randint(MINIMUM_RANDOM_NUMBER,MAXIMUM_RANDOM_NUMBER+1)
+            if random_number not in numbers:
+                numbers.append(random_number)
+
+        numbers.sort()
+        print(" ".join(f"{number:2}" for number in numbers))
