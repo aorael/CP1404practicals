b24c6d54499619a66c5cb12048230885c92bc409
Aurelia <aureliajeffrina29@gmail.com>
2025-02-26 15:06:07 +0800

    displaying certain color names and their codes in a dictionary using try-except function program DONE

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,12 +1,12 @@
 
 NAME_TO_CODE = {"Amber": "ffbf00", "Amethyst": "9966cc" ,"Apricot": "fbceb1", "Aqua": "00ffff", "Asparagus": "87a96b", "Black": "000000", "Blond": "faf0be", "Bone": "e3dac9", "Brown": "a52a2a", "Burgundy": "800020"}
 
-print(len(NAME_TO_CODE.keys()))
+# print(len(NAME_TO_CODE.keys())) # number of colors
 
 color_name = input("Enter a color: ").title()
 while color_name != "":
     try:
-        print(f"{color_name} = #{NAME_TO_CODE[color_name]}")
-    except:
-        print("The color is not in the dictionary")
+        print(f"#{NAME_TO_CODE[color_name]}")
+    except KeyError:
+        print("The color is unavailable")
     color_name = input("Enter a color: ").title()
