1fa9e28569c2e09f8374eb129c67b5747d0f762f
Aurelia <aureliajeffrina29@gmail.com>
2025-03-01 16:30:58 +0800

    Add 10 colors names which its color codes will be displayed once user input the name

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,4 +1,3 @@
-
 NAME_TO_CODE = {"Magenta": "ff00ff", "White": "ffffff" ,"Black": "000000", "Brown": "a52a2a", "Chocolate": "d2691e", "Pink": "fc0cb", "Gray": "bebebe", "Sage": "bcb88a", "Purple": "a020f0", "Burgundy": "800020"}
 
 # print(len(NAME_TO_CODE.keys())) # number of colors
