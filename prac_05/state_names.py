51c9e5d827fbf27bfbefa397c68bce6c386acfaf
Aurelia <aureliajeffrina29@gmail.com>
2025-03-01 16:45:38 +0800

    Changed some code into a better formatted code

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,7 +3,6 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
-
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
