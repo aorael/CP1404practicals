89643eb857bb872d033c1d6ecd5e0812947a5133
Aurelia <aureliajeffrina29@gmail.com>
2025-03-17 18:34:44 +0800

    Completed the first part and the last part of Guitars exercise

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,17 +1,22 @@
 class Guitar:
     def __init__(self, name, year, cost):
+        """stores instance variables"""
         self.name = name
         self.year = int(year)
         self.cost = float(cost)
 
     def __str__(self):
+        """prints out the instance variables using the formatted sentence"""
         return f"{self.name} ({self.year}): ${self.cost}"
 
     def get_age(self, current_year = 2025):
+        """count the age of the guitar"""
         return current_year - self.year
 
     def is_vintage(self):
-         return self.get_age() >= 50
+        """returns boolean value when the age of the guitar is over 50 years old"""
+        return self.get_age() >= 50
 
     def __lt__(self, other):
+        """compare two values if the first value is less than the other value"""
         return self.year < other.year
--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,15 +1,42 @@
 from guitar import Guitar
 
 def main():
+    """storing guitar details in a csv file"""
     filename = "guitars.csv"
+    guitars = read_contents(filename)
+    guitars.sort()  # this automatically sorts using __lt__ method (by year)
+    for guitar in guitars:
+        print(guitar)
+
+    get_guitar(guitars)
+    overwrite_contents(filename, guitars)
+
+def read_contents(filename):
+    """read the contents in a csv file"""
     with open(filename, "r") as in_file:
         guitars = []
         for line in in_file:
             parts = line.strip().split(",")
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
+    return guitars
 
-    guitars.sort() #this automatically sorts using __lt__ method (by year)
-    for guitar in guitars:
-        print(guitar)
+
+def overwrite_contents(filename, guitars):
+    """overwrite the contents into a csv file"""
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
+
+def get_guitar(guitars):
+    """Get guitar details from user input"""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
+        name = input("Name: ")
+
 main()
