60440ec8cac0e5008f27ce417bcd0b935feec7a4
Aurelia <aureliajeffrina29@gmail.com>
2025-03-24 17:48:01 +0800

    modified box layout demo

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,15 +4,18 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """initializes the instance variables"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout_demo.kv')
         return self.root
 
     def handle_greet(self):
+        """handles the greeting output"""
         print('test greet')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """handles clearing the input box"""
         print("test clear")
         self.root.ids.input_name.text = ""
 
