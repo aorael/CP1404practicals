70d62acb9c58afbd36fa2a5fc94f61f0b4cc6d51
Aurelia <aureliajeffrina29@gmail.com>
2025-03-29 20:59:05 +0800

    modified the sub-class, checked the objects and the drive method

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,17 @@
+from random import randint
+from car import Car
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability:float):
+        """initializes the instance variables"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def __str__(self):
+        return f"{super().__str__()}, reliability = {self.reliability}"
+
+    def drive(self, distance):
+        """drive the car with the given distance from user"""
+        random_number = randint(0,100)
+        if random_number < self.reliability:
+            return super().drive(distance)
