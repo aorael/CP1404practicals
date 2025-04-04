6c1cce1d143d7d91b00c931defb44763f41df7ed
Aurelia <aureliajeffrina29@gmail.com>
2025-04-04 20:19:12 +0800

    Created sub-class for taxi, with special service and tested it

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,19 @@
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """Class Silver Service Taxi"""
+    flagfall =4.50
+
+    def __init__(self, name, fuel, fanciness=0.0):
+        """initializes instance variables"""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        """display instance variables with the following format"""
+        return f"{super().__str__()} plus flagfall of ${SilverServiceTaxi.flagfall:.2f}"
+
+    def get_fare(self):
+        """calculate the total fare for a silver taxi"""
+        return (super().get_fare()) + self.flagfall
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,21 @@
+from silver_service_taxi import SilverServiceTaxi
+
+my_taxi = SilverServiceTaxi("Hummer", 200, 4)
+print(my_taxi)
+
+taxi2 = SilverServiceTaxi("Taxi 2", 100, 2)
+# fuel cant be less than 18 because taxi might stop
+# fuel is 100 because to avoid running out of fuel
+print(taxi2)
+
+print("- checking with assert -")
+def check_by_assert():
+    expected_fare = 48.80
+    taxi2.drive(18)
+    calculated_fare = taxi2.get_fare()
+    print(f"{calculated_fare:.2f}") # for checking
+
+    assert round(calculated_fare, 2) == expected_fare
+    # no AssertionError means the values match
+
+check_by_assert()
