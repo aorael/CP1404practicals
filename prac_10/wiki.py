8e60d16802aee0873081f247995d4c58e9e213d4
Aurelia <aureliajeffrina29@gmail.com>
2025-04-10 22:36:16 +0800

    Created a wikipedia program using the wikipedia package

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,20 +3,22 @@ import wikipedia
 MAXIMUM_SENTENCES = 5
 MAXIMUM_RESULTS = 1
 
-page_title = input("Enter page title: ").title()
+page_title = input("Enter page title: ")
 while page_title != "":
     try:
-        page = wikipedia.page(f"{page_title}")
-        wikipedia.page(f"{page_title}")
-        print("\n".join(wikipedia.search(f"{page_title}", results = MAXIMUM_RESULTS)))
-        print(wikipedia.summary(f"{page_title}", sentences = MAXIMUM_SENTENCES))
+        page = wikipedia.page(page_title, auto_suggest=False)
+        print("\n".join(wikipedia.search(page, results = MAXIMUM_RESULTS)))
+        print(wikipedia.summary(page, sentences = MAXIMUM_SENTENCES))
         print(page.url)
+
     except wikipedia.exceptions.PageError:
         print(f"Page id \"{page_title}\" does not match any pages. Try another id!")
-    except wikipedia.exceptions.DisambiguationError:
+    except wikipedia.exceptions.DisambiguationError as e:
         print("We need a more specific title. Try one of the following, or a new search:")
-        print(wikipedia.search(f"{page_title}"))
+        print("(BeautifulSoup warning)")
+        print(e.options)
+
     print()
-    page_title = input("Enter page title: ").title()
+    page_title = input("Enter page title: ")
 
 print("Thank you.")
